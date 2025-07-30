700ce72ec9618849585c87ce8a9a82c491d54e94
n4e.r <nasser432243@gmail.com>
2025-07-30 09:37:58 +0400

    added all the solutions for map1 exercises

---
+++ map1/mapAB4.java
@@ -0,0 +1,19 @@
+public Map<String, String> mapAB4(Map<String, String> map) {
+  if(map.containsKey("a") && map.containsKey("b")){
+  
+    if(map.get("a").length() == map.get("b").length()) {
+      map.put("a", "");
+      map.put("b", "");
+    }
+    
+    if(map.get("a").length() > map.get("b").length()) {
+      map.put("c", map.get("a"));
+    }
+    
+    if(map.get("b").length() > map.get("a").length()) {
+      map.put("c", map.get("b"));
+    }
+  }
+  
+  return map;
+}
---
+++ map1/mapShare.java
@@ -0,0 +1,8 @@
+public Map<String, String> mapShare(Map<String, String> map) {
+  if(map.get("a") != null){
+    map.put("b", map.get("a"));
+  }
+  map.remove("c");
+  
+  return map;
+}
